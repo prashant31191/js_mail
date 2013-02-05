5c36f650501a5c6b9c19ba8f04cb9d8f2e00071e
FominAlexandr <foma.erkek@gmail.com>
2013-02-05 19:54:45 +0400

    first

---
+++ mail/src/main/java/mailgroup/mail/App.java
@@ -0,0 +1,13 @@
+package mailgroup.mail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
